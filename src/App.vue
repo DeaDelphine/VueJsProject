e5e5e7f1feabaacfb581746ebf3464ff9dc2db07
Delphine De Alba <dealbadelphine@gmail.com>
2023-11-29 08:50:36 +0100

    new project VueJs

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <HelloWorld msg="Hello Delphine, welcome to the new world ! ! " />    
+  <Main></Main>
+  <Footer></Footer>
+</template>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+const classTitle = "greetings"
+</script>
+
+<template>
+
+<div :class="classTitle">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
